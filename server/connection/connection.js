5f3f8be1c08099d5a7d88713b1950e7cd7e78146
saad-nandoliya <saadnandoliya99@gmail.com>
2025-04-11 14:46:49 +0530

    Initial fullstack setup

--- server/connection/connection.js
+++ server/connection/connection.js
@@ -6,7 +6,7 @@ const db = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD, // Note: DB_PASS yaha use ho raha hai
   database: process.env.DB_NAME,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false, // Railway ke SSL ke liye
   },
