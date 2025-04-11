3bca97014e9ed975bc51970241dc1c5c8859ae9f
saad-nandoliya <saadnandoliya99@gmail.com>
2025-04-11 12:33:17 +0530

    Initial fullstack setup

--- server/connection/connection.js
+++ server/connection/connection.js
@@ -1,15 +1,15 @@
-const mysql = require("mysql")
-const dotenv = require("dotenv")
-dotenv.config()
+const { Pool } = require('pg');
+require('dotenv').config();
 
-
-
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
-
+const db = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, // Note: DB_PASS yaha use ho raha hai
+  database: process.env.DB_NAME,
+  port: process.env.PORT,
+  ssl: {
+    rejectUnauthorized: false, // Railway ke SSL ke liye
+  },
+});
 
 module.exports = db;
