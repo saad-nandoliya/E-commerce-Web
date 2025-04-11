e1894ed15b1930c411785c5e05364d303fbf82d1
saad-nandoliya <saadnandoliya99@gmail.com>
2025-04-11 13:02:12 +0530

    Initial fullstack setup

--- e_commerce (1).sql
+++ e_commerce (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 05, 2025 at 08:16 AM
+-- Generation Time: Apr 11, 2025 at 08:33 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -60,14 +60,6 @@ CREATE TABLE `cart_items` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Updates on modification'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `cart_items`
---
-
-INSERT INTO `cart_items` (`id`, `user_id`, `product_id`, `quantity`, `status`, `created_at`, `updated_at`) VALUES
-(217, 79, 64, 1, 'Pending', '2025-04-04 06:00:47', '2025-04-04 06:00:47'),
-(218, 79, 65, 1, 'Pending', '2025-04-04 06:00:47', '2025-04-04 06:00:47');
-
 -- --------------------------------------------------------
 
 --
@@ -88,8 +80,7 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `image`, `title`, `status`, `created_at`, `updated_at`) VALUES
-(33, '1740453226347-a.n.jpg', 'bhdjhsin', NULL, '2025-02-25 03:13:46', '2025-02-25 03:13:46'),
-(34, '1740566618456-a.d.jpg', 'rytfgio', NULL, '2025-02-26 10:43:38', '2025-02-26 10:43:38');
+(33, '1740453226347-a.n.jpg', 'Electronics', NULL, '2025-02-25 03:13:46', '2025-04-08 06:11:17');
 
 -- --------------------------------------------------------
 
@@ -104,6 +95,13 @@ CREATE TABLE `orders` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Updates on modification'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `total_amount`, `created_at`, `updated_at`) VALUES
+(0, NULL, 316871.00, '2025-04-08 11:30:09', '2025-04-08 11:30:09');
 
 -- --------------------------------------------------------
 
@@ -163,18 +161,16 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `status`, `image`, `name`, `price`, `description`, `category_id`, `created_at`, `updated_at`) VALUES
-(59, 'active', '1740652268720-a.d.jpg', 'yuiio', 879.00, '<p>uijkp[bguo</p>', 33, '2025-02-27 10:31:08', '2025-03-04 05:30:27'),
-(60, 'active', '1740652989756-a.arrow.jpg', 'hjko', 78998.00, '<p>uyiujkjl[</p>', 33, '2025-02-27 10:43:09', '2025-02-28 06:30:10'),
-(61, 'active', '1740653023523-a.d.jpg', 'tuhgjuh', 7980.00, '<p>iguhjijpokko[</p>', 34, '2025-02-27 10:43:43', '2025-02-28 06:30:12'),
-(62, 'active', '1740653622233-a.i.jpg', 'tyfgyui', 7687.00, '<p>ujikoihjhuhiojpokok</p>', 34, '2025-02-27 10:53:42', '2025-02-28 06:30:13'),
-(64, 'active', '1741063987553-a.i.jpg', 'ibnkm,', 8908.00, '<p>guihiojyuhbuihuo</p>', 33, '2025-03-04 04:53:07', '2025-03-04 05:29:19'),
-(65, 'active', '1741064000985-a.l.jpg', 'yhjkhpiom', 8909.00, '<p>uhokjpokpoko[</p>', 33, '2025-03-04 04:53:20', '2025-03-04 05:29:02'),
-(66, 'active', '1741071385070-a.arrow.jpg', 'yghjool', 78980.00, '<p>guhjhioj</p>', 33, '2025-03-04 06:56:25', '2025-03-04 06:56:25'),
-(67, 'active', '1741072405088-a.i.jpg', 'hjol', 7980.00, '<p>fyguhjop;l</p>', 33, '2025-03-04 07:13:25', '2025-03-04 07:13:25'),
-(68, 'inactive', '1741150081958-a.i.jpg', 'Saad', 234.00, '<p>asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof &nbsp;asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof &nbsp;asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof&nbsp;</p>', 33, '2025-03-05 04:48:01', '2025-04-05 05:38:15'),
-(69, 'active', '1741150129448-a.i.jpg', 'Saad', 87768.00, '<p>werwet</p>', 33, '2025-03-05 04:48:49', '2025-03-05 04:48:49'),
-(70, 'active', '1741150625315-a.d.jpg', 'fgjhkl', 8789.00, '<p>tuyjklkokihuijhilkj</p>', 33, '2025-03-05 04:57:05', '2025-03-05 04:57:05'),
-(71, 'active', '1741150673769-a.arrow.jpg', 'bjdjlnkl', 8789.00, '<p>tfyguihijjp</p>', 33, '2025-03-05 04:57:53', '2025-03-05 04:57:53');
+(59, 'active', '1740652268720-a.d.jpg', 'vivo 100', 879.00, '<p>uijkp[bguo</p>', 33, '2025-02-27 10:31:08', '2025-04-10 09:33:09'),
+(60, 'active', '1740652989756-a.arrow.jpg', 'mobile', 78998.00, '<p>uyiujkjl[</p>', 33, '2025-02-27 10:43:09', '2025-04-08 06:04:21'),
+(64, 'active', '1741063987553-a.i.jpg', 'leptop', 8908.00, '<p>guihiojyuhbuihuo</p>', 33, '2025-03-04 04:53:07', '2025-04-08 06:05:00'),
+(65, 'active', '1741064000985-a.l.jpg', 'leptop', 8909.00, '<p>uhokjpokpoko[</p>', 33, '2025-03-04 04:53:20', '2025-04-08 06:05:09'),
+(66, 'active', '1741071385070-a.arrow.jpg', 'tablet', 78980.00, '<p>guhjhioj</p>', 33, '2025-03-04 06:56:25', '2025-04-08 06:05:27'),
+(67, 'active', '1741072405088-a.i.jpg', 'tablet', 7980.00, '<p>fyguhjop;l</p>', 33, '2025-03-04 07:13:25', '2025-04-08 06:05:38'),
+(68, 'inactive', '1741150081958-a.i.jpg', 'tablet', 234.00, '<p>asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof &nbsp;asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof &nbsp;asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof asdsdf uihiosjf 3urifiwu udhgodgeo isodhfviof&nbsp;</p>', 33, '2025-03-05 04:48:01', '2025-04-08 06:05:46'),
+(69, 'active', '1741150129448-a.i.jpg', 'tablet', 87768.00, '<p>werwet</p>', 33, '2025-03-05 04:48:49', '2025-04-08 06:05:58'),
+(70, 'active', '1741150625315-a.d.jpg', 'mobile', 8789.00, '<p>tuyjklkokihuijhilkj</p>', 33, '2025-03-05 04:57:05', '2025-04-08 06:07:30'),
+(71, 'active', '1741150673769-a.arrow.jpg', 'leptop', 8789.00, '<p>tfyguihijjp</p>', 33, '2025-03-05 04:57:53', '2025-04-08 06:07:41');
 
 -- --------------------------------------------------------
 
@@ -185,6 +181,7 @@ INSERT INTO `products` (`id`, `status`, `image`, `name`, `price`, `description`,
 CREATE TABLE `shipping_addresses` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
+  `order_id` int(11) NOT NULL,
   `address` varchar(255) DEFAULT NULL,
   `city` varchar(255) DEFAULT NULL,
   `state` varchar(255) DEFAULT NULL,
@@ -218,7 +215,6 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email_or_phone`, `password`, `otp`, `picture`, `created_at`, `updated_at`) VALUES
-(78, 'asrar', 'asrarjabir786@gmail.com', '$2b$10$3hgxF4A7lUt7VQSAcsdiFebbAbs8mnh3Frbo4CcQzYSJiNwfFAbN6', 292722, '', '2025-03-20 05:32:49', '2025-03-20 05:32:49'),
 (79, 'saad', 'saadfaruk786@gmail.com', '$2b$10$bLEhxezil8qhbrnty9Vxa.cxtej/4.YiUxGC9RjKEzrr2RQBlFvQ.', 489929, '', '2025-03-20 05:45:18', '2025-03-20 05:45:18');
 
 --
@@ -280,7 +276,8 @@ ALTER TABLE `products`
 --
 ALTER TABLE `shipping_addresses`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `order_id` (`order_id`);
 
 --
 -- Indexes for table `users`
@@ -297,31 +294,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admin_users`
 --
 ALTER TABLE `admin_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `cart_items`
 --
 ALTER TABLE `cart_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=222;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=238;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 
 --
 -- Constraints for dumped tables
@@ -364,7 +361,8 @@ ALTER TABLE `products`
 -- Constraints for table `shipping_addresses`
 --
 ALTER TABLE `shipping_addresses`
-  ADD CONSTRAINT `shipping_addresses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `shipping_addresses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `shipping_addresses_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
